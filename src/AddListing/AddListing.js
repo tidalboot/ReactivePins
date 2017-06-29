616a9f54cd6eeb2363cb424b75983878d03bf3ad
Nick Jones <nickjones@Nicks-MacBook-Pro-3.local>
2017-06-29 20:08:16 +0100

    added an add feature

---
+++ src/AddListing/AddListing.js
@@ -0,0 +1,31 @@
+import React, { Component } from 'react';
+import './AddListing.css';
+
+class AddListing extends Component {
+  constructor(props) {
+    super(props);
+    this.state = { traderName: '' };
+    this.handleUpdate = this.handleUpdate.bind(this);
+    this.addListing = this.addListing.bind(this)
+  }
+  handleUpdate(event) {
+    this.setState({ traderName: event.target.value });
+  }
+
+  addListing() {
+    this.props.addListing(this.state.traderName);
+    this.setState({traderName: ''});
+  }
+
+  render() {
+    return (
+      <div className="AddListing">
+        <input type="text" placeholder="Trader name" onChange={this.handleUpdate} value={this.state.traderName}/>
+        &nbsp;&nbsp;
+        <button onClick={this.addListing}>Add Listing</button>
+      </div>
+    );
+  }
+}
+
+export default AddListing;
