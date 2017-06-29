bc8c195f641a1fded14410d56fefd0bdbbb7888f
Nick Jones <nickjones@Nicks-MacBook-Pro-3.local>
2017-06-29 20:08:04 +0100

    added an add button

--- src/Listings/Listings.js
+++ src/Listings/Listings.js
@@ -1,15 +1,30 @@
 import React, { Component } from 'react';
 import './Listings.css'
 import Listing from '../Listing/Listing'
+import AddListing from '../AddListing/AddListing'
 
 class Listings extends Component {
+  constructor(traders) {
+    super(traders);
+    this.state = {traderNames: ['Tidalboot', 'RyanCosans', 'JeffKaplan', 'ChrisMetzen']}
+    this.addListing = this.addListing.bind(this);
+  }
+
+  addListing(pinName) {
+    this.setState({traderNames: [...this.state.traderNames, pinName]});
+  }
+
+  allListings() {
+    return this.state.traderNames.map(name => (
+    <Listing key={name} name={name}/>  
+    ));
+  }
+
   render() {
     return (
       <div className="Listings">
-        Below are the currently available pins
-        <Listing name="Tidalboot"/>  
-        <Listing name="RyanCosans"/>
-        <Listing name="JeffKaplan"/>
+        <AddListing addListing={this.addListing}/>
+        {this.allListings()}
       </div>
     );
   }
